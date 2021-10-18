ea5ed43fb8823c61a5472ec908a4c321f2d29f48
ibnul364 <ayaz1873@gmail.com>
2021-10-18 19:01:52 +0600

    Navigation Drawer and Fragment

--- PlantAppUi/app/src/main/java/com/example/plantappui/MainActivity.java
+++ PlantAppUi/app/src/main/java/com/example/plantappui/MainActivity.java
@@ -1,65 +1,164 @@
 package com.example.plantappui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.plantappui.databinding.ActivityMainBinding;
+import com.example.plantappui.fragment.FavouriteFragment;
+import com.example.plantappui.fragment.HomeFragment;
+import com.example.plantappui.fragment.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
 
 public class MainActivity extends AppCompatActivity {
 
+//    private AppBarConfiguration mAppBarConfiguration;
+//    private ActivityMainBinding binding;
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+    private Toolbar toolbar;
+    private AnimatedBottomBar animatedBottomBar;
+    private FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setToolbar();
+        initViews(savedInstanceState);
+        initNavComponent();
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+
+    }
+
+    private void initNavComponent() {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()){
+                    case R.id.nav_weed:
+                        Toast.makeText(MainActivity.this,"Weed",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_insects:
+                        Toast.makeText(MainActivity.this,"Insects",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_disease:
+                        Toast.makeText(MainActivity.this,"Disease",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_products:
+                        Toast.makeText(MainActivity.this,"Products",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_help:
+                        Toast.makeText(MainActivity.this,"Help",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
             }
         });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    @SuppressLint("NonConstantResourceId")
+    private void initViews(Bundle savedInstanceState) {
+
+        animatedBottomBar = findViewById(R.id.animatedBottomBar);
+
+        if(savedInstanceState == null){
+            animatedBottomBar.selectTabById(R.id.home,true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction().replace(R.id.fragmentContainer,homeFragment).commit();
+        }
+
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab,
+                                      int newIndex, @NonNull AnimatedBottomBar.Tab newTab) {
+
+                Fragment fragment = null;
+                switch (newTab.getId()){
+
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.favourite:
+                        fragment = new FavouriteFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+
+                }
+
+                if(fragment != null){
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
+                }
+
+            }
+
+            @Override
+            public void onTabReselected(int i, @NonNull AnimatedBottomBar.Tab tab) {
+
+
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(false);
+        toggle.setToolbarNavigationClickListener(view -> drawer.openDrawer(GravityCompat.START));
+        toggle.setHomeAsUpIndicator(R.drawable.ic_drawer);
+        toggle.syncState();
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    private void setToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(0);
     }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.END);
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+
+
 }
